467151f474ae3ae6a02a3ed339f22903409e198a
Haekal2000 <haekal@hicolleagues.com>
2022-11-23 09:15:42 +0700

    problem not fixed

--- app/src/main/java/com/example/aplikasipresensi/util/ConstantData.kt
+++ app/src/main/java/com/example/aplikasipresensi/util/ConstantData.kt
@@ -2,3 +2,4 @@ package com.example.aplikasipresensi.util
 
 //const val CORE_BASE_URL = "http://192.168.100.112:3000/"
 const val CORE_BASE_URL = "http://192.168.100.56:3000/"
+//const val CORE_BASE_URL = "http://192.168.100.24:3000/"
